c4b90da842674abb043eff8b96ace0807e78e169
Nikhil kumar <143347074+Nikhilsingh46@users.noreply.github.com>
2025-05-25 12:02:19 +0530

    Time: 114 ms, Memory: 56.5 MB - LeetHub

---
+++ 2623-69-2131-longest-palindrome-by-concatenating-two-letter-words/2623-69-2131-longest-palindrome-by-concatenating-two-letter-words.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int longestPalindrome(String[] words) {
+        Map<String, Integer> wordCount = new HashMap<>();
+        int palindromeLength = 0;
+        boolean hasCentralWord = false;
+
+        for (String word : words) {
+            String reversed = new StringBuilder(word).reverse().toString();
+            if (wordCount.getOrDefault(reversed, 0) > 0) {
+                palindromeLength += 4;
+                wordCount.put(reversed, wordCount.get(reversed) - 1);
+            } else {
+                wordCount.put(word, wordCount.getOrDefault(word, 0) + 1);
+            }
+        }
+
+        for (Map.Entry<String, Integer> entry : wordCount.entrySet()) {
+            String word = entry.getKey();
+            int count = entry.getValue();
+            if (word.charAt(0) == word.charAt(1) && count > 0) {
+                palindromeLength += 2;
+                break; // Only one central word can be used
+            }
+        }
+
+        return palindromeLength;
+    }
+}
